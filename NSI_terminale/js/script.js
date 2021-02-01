ceed47b6196f2c6ef63457989aeb7246b3928a79
Olivier <olivier.elophe@gmail.com>
2021-02-01 08:25:49 +0100

    cartes

--- NSI_terminale/js/script.js
+++ NSI_terminale/js/script.js
@@ -1,6 +1,17 @@
 
 let mesCartes = document.querySelectorAll(".carte");
 
+function start(){
+    let main = document.getElementById("main");
+    for (let i=0;i<10;i++){
+        main.innerHTML += '<img class="carte" src="images/wanda.jpg" width="200">';
+    }
+
+}
+
+
+
+
 function surCarte(item){
     item.target.style.backgroundColor = "black";
     item.target.style.color = "white";
@@ -14,3 +25,4 @@ mesCartes.forEach(function (carte){
 });
 
 
+start();
