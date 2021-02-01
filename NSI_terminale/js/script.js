4f68f7ae4150e5070ec426e77f97cb64827c5131
Olivier <olivier.elophe@gmail.com>
2021-02-01 08:55:28 +0100

    avec début tableau de json

--- NSI_terminale/js/script.js
+++ NSI_terminale/js/script.js
@@ -1,25 +1,63 @@
+let jeu = [{
+        "name": "wanda",
+        "src": "images/wanda.jpg"
+    }, {
+        "name": "wanda2",
+        "src": "images/wanda2.jpg"
+    }, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}, {
+    "name": "wanda2",
+    "src": "images/wanda2.jpg"
+}];
 
-
+console.log(jeu[0].name);
 
 function start(){
     let main = document.getElementById("main");
     for (let i=0;i<10;i++){
-        main.innerHTML += '<img class="carte" src="images/wanda.jpg" width="200">';
+        main.innerHTML += '<img class="carte" src="images/dos.png" width="200">';
     }
 
 }
-
 start();
 let mesCartes = document.querySelectorAll(".carte");
-
-console.log(mesCartes);
-
 
 
 mesCartes.forEach(function (carte){
     console.log(carte.innerText);
     carte.addEventListener("click", item => {
-
         surCarte(item);
     });
 });
@@ -28,5 +66,7 @@ function surCarte(item){
     item.target.style.backgroundColor = "black";
     item.target.style.color = "white";
 
-    item.target.setAttribute('src', "images/dos.png");
+    item.target.setAttribute('src', jeu[0].src);
 }
+
+
